20f5a05dbd9f3aea4d90447669c492602d9ccc2a
AyushKumarBar <ayushrock1200@gmail.com>
2023-08-12 18:36:17 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,81 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import 'js-circle-progress'
+import 'reactjs-popup/dist/index.css';
 
-function App() {
+import './App.css'
+
+
+const BACKEND_URL = 'http://localhost:5000'; // Replace with your actual backend URL
+
+
+
+const App = () => {
   const [url, setUrl] = useState('');
-  const [sentimentScore, setSentimentScore] = useState(0);
+  const [sentiment, setSentiment] = useState('');
+  const [sentiment_score, setSentiment_score] = useState('');
+  const [subjectivity_score, setSubjectivity_score] = useState('');
+  const [text_length, setText_length] = useState('');
+
 
   const analyzeSentiment = async () => {
     try {
-      const encodedUrl = encodeURIComponent(url);
-      const response = await axios.get(`/api/analyze?url=${encodedUrl}`);
-      setSentimentScore(response.data.analysis.score);
+      const response = await fetch(`${BACKEND_URL}/api/sentiment?url=${encodeURIComponent(url)}`);
+      const data = await response.json();
+      setSentiment(data.sentiment);
+      setSentiment_score(data.sentiment_score);
+      setSubjectivity_score(data.subjectivity_score);
+      setText_length(data.text_length)
+      setUrl('');
     } catch (error) {
       console.error('Error analyzing sentiment:', error);
     }
+
   };
 
+
+
   return (
-    <div>
-      <h1>Website Sentiment Analyzer</h1>
-      <input
-        type="text"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-        placeholder="Enter website URL"
-      />
-      <button onClick={analyzeSentiment}>Analyze Sentiment</button>
-      <div>
-        <p>Sentiment Score: {sentimentScore}</p>
+    <div className="main-container">
+      <h1>Website Analysis</h1>
+
+
+      <div className='input-container'>
+        <input
+          className='input'
+          type="text"
+          placeholder="Enter URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button className='submit-btn' onClick={analyzeSentiment}>Analyze</button>
       </div>
-    </div>
+      <div className='contain'>
+        <div className="result">
+          <pre>
+            <p>Sentiment         : {sentiment}</p>
+            <p>Sentiment Score   : {Math.ceil(sentiment_score * 100)}</p>
+            <p>Subjectivity      : {Math.ceil(subjectivity_score * 100)}</p>
+            <p>Words             : {text_length}</p>
+          </pre>
+        </div>
+
+        <div className='progress'>
+          {
+            sentiment_score * 100 < 0 ?
+              <>
+                <div className='red'><circle-progress anticlockwise="true" value={-Math.ceil(sentiment_score * 100)} max="100" ></circle-progress></div></>
+              :
+              <><div className='normal'><circle-progress value={Math.abs(Math.ceil(sentiment_score * 100))} max="100" ></circle-progress></div></>
+          }
+        </div>
+        <div className='normal'>
+          <circle-progress value={Math.ceil(subjectivity_score * 100)} max="100"></circle-progress>
+        </div>
+
+      </div>
+
+    </div >
   );
-}
+};
 
 export default App;
